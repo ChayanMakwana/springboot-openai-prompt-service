f492aadbb3055cfbd47cbaedbcf5e5b9e7129e49
ChayanMakwana <chayanmak7@gmail.com>
2025-07-31 22:48:22 +0530

    Initial commit - Spring Boot OpenAI Integration(Prompt Service)

--- src/main/java/com/promptservice/SpringBootOpenAIPromptServiceApplication.java
+++ src/main/java/com/promptservice/SpringBootOpenAIPromptServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootOpenAiPromptServiceApplication {
+public class SpringBootOpenAIPromptServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootOpenAiPromptServiceApplication.class, args);
+		SpringApplication.run(SpringBootOpenAIPromptServiceApplication.class, args);
 	}
 
 }
